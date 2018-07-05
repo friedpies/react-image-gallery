948024a0a3baf9887275b8acb96a4b6c2c470d23
friedpies <kenmarut@gmail.com>
2018-07-04 21:07:05 -0500

    Added search button

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = props =>
 <nav className="main-nav">
   <ul>
+    <li><Link to="/search">Search</Link></li>
     <li><a href='#'>Cats</a></li>
     <li><a href='#'>Dogs</a></li>
     <li><a href='#'>Computers</a></li>
