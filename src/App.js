4ecf72c336802bfbda90470d26cb0af96506e58d
friedpies <kenmarut@gmail.com>
2018-07-05 10:14:18 -0500

    Added base URL call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
 import Container from './Container';
-import {
-  BrowserRouter,
-  Route
-} from 'react-router-dom';
+import axios from 'axios';
+import apiKey from './config';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      photos: [],
+      loading: true
+    };
+  }
+
+componentDidMount() {
+  axios.get(`https://api.flickr.com/services/rest/?
+    method=flickr.photos.search&
+    api_key=${apiKey}&
+    tags=cats&
+    per_page=10&
+    format=json&
+    nojsoncallback=1`)
+    .then(response => {
+      this.setState({
+        photos: response.data.photos.photo,
+        loading: false
+      })
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    });
+}
+
   render() {
+    console.log(this.state.photos);
     return (
       <Container />
     );
